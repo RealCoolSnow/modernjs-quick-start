edec10243b24080a2952c3aba8f8540750ca1720
CoolSnow <670696652@qq.com>
2021-11-02 10:46:08 +0800

    feat: 启用tailwindcss 和 unbundle

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,14 @@
 import { Switch, Route } from '@modern-js/runtime/router';
 
-import './App.css';
+import '@/styles/main.css';
 
 const App = () => (
   <Switch>
     <Route exact={true} path="/">
       <div className="container">
         <main>
-          <div className="logo">
-            <img
-              src="https://lf3-static.bytednsdoc.com/obj/eden-cn/ylaelkeh7nuhfnuhf/modernjs-cover.png"
-              width="300"
-              alt="Modern.js Logo"
-            />
-          </div>
-          <p className="description">
-            Get started by editing <code className="code">src/App.tsx</code>
-          </p>
-          <div className="grid">
-            <a href="https://modernjs.dev/docs/start" className="card">
-              <h2>Quick Start</h2>
-            </a>
-            <a href="https://modernjs.dev/docs/guides" className="card">
-              <h2>Handbook</h2>
-            </a>
-            <a href="https://modernjs.dev/docs/apis" className="card">
-              <h2>API Reference </h2>
-            </a>
-            <a
-              href="https://modernjs.dev/docs/community"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="card">
-              <h2>Community </h2>
-            </a>
-          </div>
+          <p className="text-red-500">Hello world!</p>
         </main>
-        <footer className="footer">
-          <a
-            href="https://modernjs.dev"
-            target="_blank"
-            rel="noopener noreferrer">
-            Powered by Modern.js
-          </a>
-        </footer>
       </div>
     </Route>
     <Route path="*">
