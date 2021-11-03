8e680adc24eacaa7b1ff76e5ddd93f22b4bc4152
CoolSnow <670696652@qq.com>
2021-11-03 17:02:02 +0800

    feat: add images

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import intl from 'react-intl-universal';
+import { Link } from '@modern-js/runtime/router';
 import Header from '@/components/Header';
+import LocaleSwitch from '@/components/LocaleSwitch';
+import Footer from '@/components/Footer';
+import earthSvg from '@/assets/svg/earth.svg';
 
 const About = () => (
-  <div>
+  <div className="flex flex-col items-center">
     <Header title={intl.get('about')} />
-    <div>about</div>
+    <LocaleSwitch />
+    <div className="mt-6">
+      <Link to="/">
+        <span className="text-blue-500">{intl.get('home')}</span>
+      </Link>
+      <span className="p-1">|</span>
+      <span>{intl.get('about')}</span>
+    </div>
+    <img src={earthSvg} className="mt-4" />
+    <h1 className="font-sans text-lg text-gray-800 text-center">
+      {intl.get('about_msg')}
+    </h1>
+    <Footer />
   </div>
 );
 export default About;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import intl from 'react-intl-universal';
+import { Link } from '@modern-js/runtime/router';
 import LocaleSwitch from '../components/LocaleSwitch';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 
-const Index = () => (
-  <div>
-    <Header title={intl.get('home')} />
-    <LocaleSwitch />
-    <Footer />
-  </div>
-);
+const Index = () => {
+  const [count, setCount] = useState(0);
+  const onInc = () => setCount(count + 1);
+  return (
+    <div className="flex flex-col items-center">
+      <Header title={intl.get('home')} />
+      <LocaleSwitch />
+      <div className="mt-6">
+        <span>{intl.get('home')}</span>
+        <span className="p-1">|</span>
+        <Link to="/about">
+          <span className="text-blue-500">{intl.get('about')}</span>
+        </Link>
+      </div>
+      <div className="mt-6">
+        <div className="border-2 px-1 m-2 text-red-400" onClick={onInc}>
+          counter - {count}
+        </div>
+        <div className="border-2 px-1 m-2 text-blue-400">mock test</div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 export default Index;
